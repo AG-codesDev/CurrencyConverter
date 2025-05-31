f5f0f63acc1d287a472bc6b49d86a5cfd10fb600
Apurva Gaurav <app.gaurav19@gmail.com>
2025-05-31 22:45:13 +0530

    added some functionality

--- src/Components/Converter.jsx
+++ src/Components/Converter.jsx
@@ -1,6 +1,41 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const Converter = () => {
+  const currencyAPI =
+    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json";
+
+  const [fromCurrency, setFromCurrency] = useState("usd");
+  const [toCurrency, setToCurrency] = useState("inr");
+  const [amount, setAmount] = useState(0);
+  const [currencyTypes, setCurrencyTypes] = useState([]);
+
+  const handleFromCurrencyChange = (e) => {
+    setFromCurrency(e.target.value);
+  };
+
+  const handleToCurrencyChange = (e) => {
+    setToCurrency(e.target.value);
+  };
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+  const handleConvertBtnClick = () => {
+    console.log(amount);
+    console.log(fromCurrency);
+    console.log(toCurrency);
+  };
+
+  const getAllCurrencies = async () => {
+    const res = await fetch(currencyAPI);
+    const data = await res.json();
+    setCurrencyTypes(data);
+  };
+
+  useEffect(() => {
+    getAllCurrencies();
+  }, []);
+
   return (
     <section className="flex flex-col bg-gray-200 h-4/5 w-1/4 rounded-2xl justify-between p-3 items-center ">
       <h2 className="text-center font-bold text-3xl mt-4 text-blue-600">
@@ -15,8 +50,14 @@ const Converter = () => {
             name="fromCurrency"
             id="fromCurrency"
             className=" text-lg p-3 bg-gray-300 rounded-lg"
+            onChange={handleFromCurrencyChange}
           >
-            <option value="inr">INR</option>
+            {currencyTypes &&
+              Object.keys(currencyTypes).map((cur) => (
+                <option value={cur} key={cur}>
+                  {cur} - {currencyTypes[cur]}
+                </option>
+              ))}
           </select>
         </div>
 
@@ -28,18 +69,30 @@ const Converter = () => {
             name="toCurrency"
             id="toCurrency"
             className=" text-lg p-3 bg-gray-300 rounded-lg"
+            onChange={handleToCurrencyChange}
           >
-            <option value="usd">USD</option>
+            {currencyTypes &&
+              Object.keys(currencyTypes).map((cur) => (
+                <option value={cur} key={cur}>
+                  {cur} - {currencyTypes[cur]}
+                </option>
+              ))}
           </select>
         </div>
         <input
           type="text"
           className="Amount bg-gray-300 p-3 focus:outline-none rounded-lg my-5"
           placeholder="Amount"
+          onChange={handleAmountChange}
+          value={amount}
         />
       </div>
       <div className="btn-result flex flex-col items-center gap-5">
-        <button className="bg-green-600 font-bold text-white p-3 rounded-lg cursor-pointer">
+        <button
+          className="bg-green-600 font-bold text-white p-3 rounded-lg cursor-pointer"
+          onClick={handleConvertBtnClick}
+          //   onClick={printFromCurrency}
+        >
           Convert {"INR"} to {"USD"}
         </button>
         <span className="Result text-lg">
